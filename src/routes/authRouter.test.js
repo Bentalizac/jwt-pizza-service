99d5c639d5dc1b68c838bb7f3dfe1bc9229952e2
Eli Ellsworth <bentalizac@gmail.com>
2024-10-01 22:03:02 -0600

    More lint

--- src/routes/authRouter.test.js
+++ src/routes/authRouter.test.js
@@ -1,6 +1,7 @@
 const request = require('supertest');
 const app = require('../service');
 
+const { _, DB } = require('../database/database.js');
 const testUser = { name: 'pizza diner', email: 'reg@test.com', password: 'a' };
 let testUserAuthToken;
 
@@ -15,7 +16,7 @@ async function clearDB() {
 beforeAll(async () => {
 
   clearDB()
-  
+
   testUser.email = Math.random().toString(36).substring(2, 12) + '@test.com';
   const registerRes = await request(app).post('/api/auth').send(testUser);
   testUserAuthToken = registerRes.body.token;
